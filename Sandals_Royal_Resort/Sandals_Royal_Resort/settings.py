fc9d9a367e53319df05e84c3696c27e5dbf2391c
alidaashok@gmail.com <alidaashok@gmail.com>
2022-08-02 00:43:08 -0700

    heroku connected

--- Sandals_Royal_Resort/Sandals_Royal_Resort/settings.py
+++ Sandals_Royal_Resort/Sandals_Royal_Resort/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-x0d=fxe*d)g0g+usf&_qf6@xgo8!0-jr5rhuyx@z18jbtfai7k
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
